8964334248240c2911eaff1c3c0747b7ac2fd813
Irina <isblindness@gmail.com>
2016-02-24 20:28:34 +0600

    new distance, test

--- pharmacy_search/pharmacy_search/pharmacy_search.py
+++ pharmacy_search/pharmacy_search/pharmacy_search.py
@@ -1,5 +1,24 @@
-import csv
+﻿import csv
+import math
 
+def distance(apteka, x, y):
+    long_rad1 = apteka["longitude"]*math.pi/180.
+    lat_rad1 = apteka["latitude"]*math.pi/180.
+    long_rad2 = float(x)*math.pi/180.
+    lat_rad2 = float(y)*math.pi/180.
+    cl1 = math.cos(lat_rad1)
+    cl2 = math.cos(lat_rad2)
+    sl1 = math.sin(lat_rad1)
+    sl2 = math.sin(lat_rad2)
+    delta = long_rad2 - long_rad1
+    cdelta = math.cos(delta)
+    sdelta = math.sin(delta)
+    y1 = math.sqrt(math.pow(cl2*sdelta,2)+math.pow(cl1*sl2-sl1*cl2*cdelta,2))
+    x1 = sl1*sl2+cl1*cl2*cdelta
+    ad = math.atan2(y1,x1)
+    dist = ad*rad
+    return dist
+ 
 def readfile(filename):
     ap_list = []
     f = open(filename, 'r', encoding="UTF-8")
@@ -13,17 +32,18 @@ def readfile(filename):
     f.close()
     return ap_list
 
-def distance(apteka, x, y):
-    res = ((apteka["longitude"]-float(x))**2+(apteka["latitude"]-float(y))**2)**0.5
-    return res
+
 
 def count_distances(ap_list, x_coord, y_coord):
     for i in range(len(ap_list)):
         ap_list[i]["distance"] = distance(ap_list[i], x_coord, y_coord)
 
-filename = input("Name of file: ")
-x_coord = input("Longitude: ")
-y_coord = input("Latitude ")
+test_file = open("test2.txt", 'r', encoding="UTF-8")
+filename = str(test_file.readline())
+filename = filename[:len(filename)-1]
+x_coord = float(test_file.readline())
+y_coord = float(test_file.readline())
+test_file.close()
 
 apt_list = readfile(filename)
 count_distances(apt_list, x_coord, y_coord)
